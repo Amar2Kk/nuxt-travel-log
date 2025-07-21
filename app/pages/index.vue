ebc03e10e0eeeeec38460095ac191a29c433bc44
Amar2Kk <amarabas9733@gmail.com>
2025-07-21 18:09:45 +0300

    :sparkles: Added homepage

---
+++ app/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-4 rounded-2xl shadow-lg">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <div class="flex items-center justify-center">
+          <Icon name="tabler:map-2" class="inline-block mr-2 text-primary-content" size="50"/>
+          <h1 class="text-5xl font-bold text-accent">
+            Travel Log
+          </h1>
+        </div>
+        <p class="py-6">
+          Keep track of your travels and adventures with our easy-to-use travel log app. Document your journeys, share
+          experiences, and create lasting memories.
+        </p>
+        <button class="btn btn-primary">
+          Sign in with Github
+          <Icon name="tabler:brand-github" size="22" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
