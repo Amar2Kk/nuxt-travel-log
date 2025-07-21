748bd9b6e147624c1c27377c4ac1ebfed04ea2ec
Ammar Abbas <amarabas9733@gmail.com>
2025-07-21 18:12:35 +0300

    :sparkles: Added homepage (#50)
    * :screwdriver: Upgrade nuxt to 4.0.1
    
    * :sparkles: Added homepage

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold">
-      Hello World
-    </h1>
-    <button class="btn btn-secondary">
-      <Icon name="tabler:map" />
-      View Map
-    </button>
-  </div>
+  <NuxtLayout data-theme="silk">
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex min-h-screen flex-col bg-primary/90">
+    <AppNavBar />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
